487e8ad37a341e1623761897816aaec4c795aa54
Steven Lass <steven.lass@atos.net>
2019-12-03 13:33:07 -0800

    login popup = true

--- customccp/ccp1/ccp1.js
+++ customccp/ccp1/ccp1.js
@@ -140,7 +140,7 @@ ccpLogger("initCCP: start");
 
 connect.core.initCCP(containerDiv, {
     ccpUrl: ccpUrl,
-    loginPopup: false,
+    loginPopup: true,
     softphone: {
         allowFramedSoftphone: true
     }
